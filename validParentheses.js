816374f9dbca7abed60adb3939f596420fa666e8
Guilherme Davi <guiionfire@hotmail.com>
2019-06-23 20:44:21 -0300

    more katas

--- validParentheses.js
+++ validParentheses.js
@@ -1,23 +1,22 @@
+/* eslint-disable no-unused-expressions */
 const validParentheses = (parens) => {
-  if (parens[parens.length - 1] === '(' || parens[0] === ')' || parens.length % 2 !== 0) return false;
-  const parentheses = {};
-  for (let i = 0; i < parens.length; i += 1) {
-    if (parentheses[parens[i]]) {
-      parentheses[parens[i]] += 1;
-    } else parentheses[parens[i]] = 1;
+  if (parens[0] === ')' || parens[parens.length - 1] === '(' || parens.length % 2 !== 0) {
+    return false;
   }
-  return parentheses['('] === parentheses[')'];
+  const stack = [];
+  const parensCopy = [...parens].slice();
+  for (let i = 0; i < parensCopy.length; i += 1) {
+    if (parensCopy[i] === '(') {
+      stack.unshift(parensCopy.shift());
+    } else if (parensCopy[i] === ')') {
+      stack
+    }
+  }
+  console.log(stack);
 };
 
-// function validParentheses(parens){
-//   let count = 0;
-//   for (let i = 0 ; i < parens.length && count >= 0; i++) {
-//     count += (parens[i] == '(') ? 1 : - 1;
-//   }
-//   return count === 0;
-// }
-
-console.log(validParentheses('()()((()'));
+console.log(validParentheses('()'));
+// console.log(validParentheses('()())(()')); BUG
 console.log(validParentheses('())'));
 console.log(validParentheses(')())'));
 console.log(validParentheses('(()('));
